6f1100d098b05aaa32bd013c389aafa2b873d0f8
pokify <solparkmedia@proton.me>
2026-09-21 21:56:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ async def art(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
                 await update.message.reply_photo(
                     photo=photo,
-                    caption=f'₍ᵔ.  ̫.ᵔ₎ﾉ <a href="{MORE_ART_URL}">more art</a>',
+                    caption=f'₍ᵔ.  ̫.ᵔ₎ﾉ <a href="{MORE_ART_URL}">gallery</a>',
                     parse_mode="HTML"
                 )
 
